b89886f9c4143e8798566b53621a30ae9e5d4c2d
William Pollock <141882967+w-pollock@users.noreply.github.com>
2025-03-13 13:13:25 -0500

    Update ProblemaP1.java

--- ProblemaP1.java
+++ ProblemaP1.java
@@ -1,6 +1,6 @@
 //AUTORES:
 //WILLIAM POLLOCK, 202221321
-//NICOLÁS HERNÁNDEZ, {CODIGO}
+//NICOLÁS HERNÁNDEZ, 202322148
 
 import java.util.Scanner;
 import java.util.Arrays;
@@ -8,52 +8,44 @@ import java.util.Arrays;
 public class ProblemaP1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); 
+        int W = sc.nextInt(); 
 
-        for(int testCase = 0; testCase < T; testCase++) {
+        for(int casosPrueba = 0; casosPrueba < W; casosPrueba++) {
             int n = sc.nextInt();
             int j = sc.nextInt();
             int m = sc.nextInt();
 
-            
             int[] arr = new int[n + 1];
             for (int i = 1; i <= n; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            // DP(i, r, s): mínima suma al escoger r elementos de los
-            // primeros i jugadores, gastando exactamente s swaps.
-            // Dimensiones: (n+1) x (j+1) x (m+1)
-            int INF = Integer.MAX_VALUE / 2;
-            int[][][] DP = new int[n+1][j+1][m+1];
+            
+            int infinito = Integer.MAX_VALUE; 
+            int[][][] DP = new int[ n+1][j+ 1][m+1];
+            DP[0][0][0] = 0;
 
 
+
+            
             for (int i = 0; i <= n; i++) {
-                for (int r = 0; r <= j; r++) {
-                    Arrays.fill(DP[i][r], INF);
+                for (int k = 0; k <= j; k++) {
+                    Arrays.fill(DP[i][k], infinito);
                 }
             }
 
-            // Caso base: no hemos tomado ningún elemento (r=0)
-            // y no hemos gastado swaps (s=0)
-            DP[0][0][0] = 0;
 
-            // Llenar la DP
             for (int i = 1; i <= n; i++) {
-                for (int r = 0; r <= Math.min(i, j); r++) {
-                    for (int sUsed = 0; sUsed <= m; sUsed++) {
-                        // 1) NO escoger al jugador i
-                        DP[i][r][sUsed] = Math.min(DP[i][r][sUsed], DP[i-1][r][sUsed]);
-
-                        // 2) Escoger al jugador i (si r>0)
-                        if (r > 0) {
-                            // Costo de mover el jugador i a la posición r sin alterar el orden relativo
-                            int cost = (i - r); 
-                            if (sUsed >= cost) {
-                                int prevSwaps = sUsed - cost;
-                                if (DP[i-1][r-1][prevSwaps] != INF) {
-                                    int candidate = DP[i-1][r-1][prevSwaps] + arr[i];
-                                    DP[i][r][sUsed] = Math.min(DP[i][r][sUsed], candidate);
+                for (int k = 0; k <= Math.min(i, j); k++) {
+                    for (int sUsados = 0; sUsados <= m; sUsados++) {
+                        DP[i][k][sUsados] = Math.min(DP[i][k][sUsados], DP[i-1][k][sUsados]);
+                        if (k > 0) {
+                            int costoIntercambio = (i - k); 
+                            if (sUsados >=costoIntercambio) {
+                                int intercAnteriores = sUsados -costoIntercambio;
+                                if (DP[i-1][k-1][intercAnteriores] != infinito) {
+                                    int jugEscogido = DP[i-1][k-1][intercAnteriores] + arr[i];
+                                    DP[i][k][sUsados] = Math.min(DP[i][k][sUsados], jugEscogido);
                                 }
                             }
                         }
@@ -61,14 +53,12 @@ public class ProblemaP1 {
                 }
             }
 
-            // Tomar la mejor respuesta (suma mínima) para r=j y 0 <= s <= m
-            int ans = INF;
-            for (int sUsed = 0; sUsed <= m; sUsed++) {
-                ans = Math.min(ans, DP[n][j][sUsed]);
+            int respuesta = infinito;
+            for (int sUsados = 0; sUsados <= m; sUsados++) {
+                respuesta = Math.min(respuesta, DP[n][j][sUsados]);
             }
 
-            // Imprimir el resultado
-            System.out.println(ans);
+            System.out.println(respuesta);
         }
 
         sc.close();
